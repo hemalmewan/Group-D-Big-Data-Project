bb01d673bfb74e9b87873a86d8c359bf26f39ced
Hemal Mewantha <105871416+hemalmewan@users.noreply.github.com>
2025-08-08 21:46:15 +0530

    Add files via upload

---
+++ project_iam_permission/variable.tf
@@ -0,0 +1,13 @@
+## define the variables
+
+## define the project ID
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+  type        = string
+}
+
+## define the user email for DS 4004
+variable "ds_4004_user_email" {
+  description = "The email of the user for DS 4004"
+  type        = string
+}
